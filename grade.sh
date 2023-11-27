dcbc8d28cf2eab8d3761d5ac89b75ed6af0b1b73
Megalotrousle <tangziheng0328@gmail.com>
2023-11-27 13:01:32 -0800

    modified grade.sh

--- grade.sh
+++ grade.sh
@@ -1,9 +1,12 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
 
 mkdir grading-area
+mkdir student-submission
+
+homeDir=`pwd`
 
 git clone $1 student-submission
 echo 'Finished cloning'
@@ -14,15 +17,27 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
-cp -r lab4/*Tests.java grading-area
+cp -r TestListExamples.java grading-area
+cp -r student-submission/ListExamples.java grading-area
 
 tests=`find student-submission -name "*.java"`
-# for file in tests; do
+
+for file in $tests; do
     if [ -f $file ]; then
-        echo "passed"
+        echo "File found."
     else
         echo "Wrong kind of file for $file"
         exit 1
     fi
 done
 
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+
+for file in $tests; do
+    if [ $? -ne 0 ]; then
+        echo "Compile error."
+    else
+        echo "passed"
+    fi
+done
